c2f16108c3cf1c4d9dafcb4918c4af4ea0e611aa
stanislav <rebel1866@yandex.ru>
2023-01-18 00:31:18 +0300

    animation done

--- js/home.js
+++ js/home.js
@@ -1,89 +1,74 @@
 document.addEventListener("DOMContentLoaded", init);
 let count = 0;
 let left = true;
+let timerID;
 
 function init() {
-  let btn = document.getElementById("btn");
-  let btnR = document.getElementById("btnR");
+  initSlides();
+}
 
-  document.getElementById("second").style.transform = "translateX(-100%)";
-  document.getElementById("first").style.transform = "translateX(-200%)";
-  document.getElementById("third").style.transform = "translateX(0%)";
-
-  function goLeft() {
-    if (count === 2) {
-      return;
-    }
-    count++;
-    let thirdEl = document.getElementById("third");
-    let secEl = document.getElementById("second");
-    let firstEl = document.getElementById("first");
-
-    let third = thirdEl.style.transform;
-    let second = secEl.style.transform;
-    let first = firstEl.style.transform;
-
-    let transformThird = Number(extractTransformNum(third));
-    let transformSecond = Number(extractTransformNum(second));
-    let transformFirst = Number(extractTransformNum(first));
-
-    let newTransformThird = transformThird + 100;
-    let newTransformSec = transformSecond + 100;
-    let newTransformFir = transformFirst + 100;
-
-    firstEl.style.transform = "translateX(" + newTransformFir + "%)";
-    secEl.style.transform = "translateX(" + newTransformSec + "%)";
-    thirdEl.style.transform = "translateX(" + newTransformThird + "%)";
+function initSlides() {
+  let slides = document.getElementsByClassName("slide");
+  for (let i = 0; i < slides.length; i++) {
+    let percent = -(i * 100);
+    let slide = slides[i];
+    slide.style.transform = `translateX(${percent}%)`;
   }
-  function goRight() {
-    if (count === 0) {
-      return;
-    }
-    count--;
-    let thirdEl = document.getElementById("third");
-    let secEl = document.getElementById("second");
-    let firstEl = document.getElementById("first");
-
-    let third = thirdEl.style.transform;
-    let second = secEl.style.transform;
-    let first = firstEl.style.transform;
-
-    let transformThird = Number(extractTransformNum(third));
-    let transformSecond = Number(extractTransformNum(second));
-    let transformFirst = Number(extractTransformNum(first));
-
-    let newTransformThird = transformThird - 100;
-    let newTransformSec = transformSecond - 100;
-    let newTransformFir = transformFirst - 100;
-
-    firstEl.style.transform = "translateX(" + newTransformFir + "%)";
-    secEl.style.transform = "translateX(" + newTransformSec + "%)";
-    thirdEl.style.transform = "translateX(" + newTransformThird + "%)";
-  }
-
-  btn.addEventListener("click", goLeft);
-  btnR.addEventListener("click", goRight);
+  document.getElementById("left").addEventListener("click", goLeft);
+  document.getElementById("right").addEventListener("click", goRight);
   goLeftRight();
+}
 
-  function goLeftRight() {
-    setInterval(function () {
-      if (left) {
-        goLeft();
-        if (count === 2) {
-          left = false;
-        }
-      } else {
-        goRight();
-        if (count === 0) {
-          left = true;
-        }
+function goLeft() {
+  clearInterval(timerID);
+  if (count === 8) {
+    return;
+  }
+  count++;
+
+  let slides = document.getElementsByClassName("slide");
+  for (let slide of slides) {
+    let transformOld = Number(extractTransformNum(slide.style.transform));
+    let transformNew = transformOld + 100;
+    slide.style.transform = `translateX(${transformNew}%)`;
+  }
+  goLeftRight();
+}
+
+function goRight() {
+  clearInterval(timerID);
+  if (count === 0) {
+    return;
+  }
+  count--;
+
+  let slides = document.getElementsByClassName("slide");
+  for (let slide of slides) {
+    let transformOld = Number(extractTransformNum(slide.style.transform));
+    let transformNew = transformOld - 100;
+    slide.style.transform = `translateX(${transformNew}%)`;
+  }
+  goLeftRight();
+}
+
+function extractTransformNum(transformStr) {
+  transformStr = transformStr.substring(11);
+  let transformStrLenght = transformStr.length;
+  return transformStr.substring(0, transformStrLenght - 2);
+}
+
+function goLeftRight() {
+  timerID = setInterval(function () {
+    if (left) {
+      goLeft();
+      if (count === 8) {
+        left = false;
       }
-    }, 3000);
-  }
-
-  function extractTransformNum(transformStr) {
-    transformStr = transformStr.substring(11);
-    let transformStrLenght = transformStr.length;
-    return transformStr.substring(0, transformStrLenght - 2);
-  }
+    } else {
+      goRight();
+      if (count === 0) {
+        left = true;
+      }
+    }
+  }, 3000);
 }
