1e05ccdc8a489eca69a350d1ef282ff4149033cb
stanislav <rebel1866@yandex.ru>
2023-01-28 18:42:03 +0300

    catalog

--- js/infinitScroll.js
+++ js/infinitScroll.js
@@ -1,10 +1,33 @@
 let infiniteScroll = (function () {
   let isExecuted = false;
-  let page = 1;
+  let page = 2;
+  const pattern =
+    '<div class="item">   <div class="itemPhoto">      <img />    </div><div class="iName iHeader"></div><div class="iAuthor iHeader"></div><div class="iPrice iHeader"></div><button class="getBtn">Buy it now!</button>  </div>';
 
   function init() {
+    fetch(`http://localhost:3000/paintings?_limit=16&_page=1`)
+      .then((response) => response.json())
+      .then((result) => fillPaintings(result));
     document.addEventListener("scroll", handle);
   }
+
+  function fillPaintings(data) {
+    for (let el of data) {
+      let inner = document.getElementById("items").innerHTML;
+      document.getElementById("items").innerHTML = inner + pattern;
+      fillLastEl(el);
+    }
+  }
+
+  function fillLastEl(element) {
+    let lastItem = document.getElementById("items").lastElementChild;
+    lastItem.querySelector(".itemPhoto img").setAttribute("src", element.img);
+    lastItem.setAttribute("id", element.id);
+    lastItem.querySelector(".iName").innerHTML = element.name;
+    lastItem.querySelector(".iAuthor").innerHTML = element.author;
+    lastItem.querySelector(".iPrice").innerHTML = element.price + "$";
+  }
+
   function handle() {
     if (isExecuted) {
       return;
@@ -42,11 +65,7 @@ let infiniteScroll = (function () {
     }
     isExecuted = false;
 
-    for (let item of data) {
-      let newItem = document.createElement("div");
-      newItem.classList.add("item");
-      document.getElementById("items").appendChild(newItem);
-    }
+    fillPaintings(data);
 
     let btn = document.createElement("button");
     btn.setAttribute("id", "showMore");
