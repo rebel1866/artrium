ace787bc33f6b4476aced39a6cd25400bdda0591
melnikov <melnikov@isida.by>
2023-02-03 17:48:35 +0300

    cart dynamiclaly

--- js/cart.js
+++ js/cart.js
@@ -1,4 +1,7 @@
 let cart = (function () {
+
+  let car = '<div class="cartItem cartEl"><img class="cartImage cartEl" src="|imgPath|" ><div class="cartTitle cartEl cartHeader">|name|</div> <div class="cartAuthor cartEl cartHeader">|author|</div><div class="cartPrice cartEl cartHeader">|price|$</div></div>';
+  
   function init() {
     document.getElementById("cartForm").style.display = "none";
     document.getElementById("cart").addEventListener("mouseover", () => {
@@ -14,6 +17,26 @@ let cart = (function () {
       document.getElementById("cartForm").style.display = "none";
       }
     });
+    render();
   }
+
+
+ function render(){
+  let obj = {name:'The scream of the butterfly',author:'Vincent van gogh',price:1200, img:'img/3.jpeg'}
+  let rootElement = document.getElementById('cartForm');
+  rootElement.innerHTML = "";
+  
+  let elementPattern = car;
+ elementPattern  = elementPattern.replace('|name|',obj.name);
+ elementPattern =  elementPattern.replace('|price|',obj.price);
+ elementPattern = elementPattern.replace('|imgPath|',obj.img);
+ elementPattern = elementPattern.replace('|author|',obj.author);
+
+  for(let i=0; i<3; i++){
+    rootElement.innerHTML = rootElement.innerHTML + elementPattern;
+  }
+  rootElement.innerHTML = rootElement.innerHTML + '<button id="getAll" class="cartEl">GET IT ALL!</button>';
+ }
+
   return { init: init };
 })();
