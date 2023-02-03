bef76414d4d98379c4023e1379cee1934b05256c
stanislav <rebel1866@yandex.ru>
2023-02-03 20:29:17 +0300

    cartForm

--- js/cart.js
+++ js/cart.js
@@ -1,42 +1,62 @@
 let cart = (function () {
+  const elementPattern =
+    '<div class="cartItem cartEl"><img class="cartImage cartEl" src="|imgPath|" ><div class="cartTitle cartEl cartHeader">|name|</div> <div class="cartAuthor cartEl cartHeader">|author|</div><div class="cartPrice cartEl cartHeader">|price|$</div></div>';
 
-  let car = '<div class="cartItem cartEl"><img class="cartImage cartEl" src="|imgPath|" ><div class="cartTitle cartEl cartHeader">|name|</div> <div class="cartAuthor cartEl cartHeader">|author|</div><div class="cartPrice cartEl cartHeader">|price|$</div></div>';
-  
+  const cartAfter =
+    ' #cart::after {content: "|amount|";font-weight: bold;background: dodgerblue;border-radius: 50%; width: 10px;   margin-top: -10px;margin-left: -30px; font-size: 13px;padding: 2px }';
+
   function init() {
     document.getElementById("cartForm").style.display = "none";
     document.getElementById("cart").addEventListener("mouseover", () => {
       document.getElementById("cartForm").style.display = "block";
     });
     document.getElementById("cart").addEventListener("mouseout", (e) => {
-      if (e.relatedTarget.getAttribute("id") !== "cartForm" && !e.relatedTarget.classList.contains('cartItem')) {
+      if (
+        e.relatedTarget.getAttribute("id") !== "cartForm" &&
+        !e.relatedTarget.classList.contains("cartItem")
+      ) {
         document.getElementById("cartForm").style.display = "none";
       }
     });
     document.getElementById("cartForm").addEventListener("mouseout", (e) => {
-      if ( e.relatedTarget !== null && !e.relatedTarget.classList.contains('cartEl')) {
-      document.getElementById("cartForm").style.display = "none";
+      if (
+        e.relatedTarget !== null &&
+        !e.relatedTarget.classList.contains("cartEl")
+      ) {
+        document.getElementById("cartForm").style.display = "none";
       }
     });
     render();
   }
 
+  function render() {
+    let rootElement = document.getElementById("cartForm");
+    rootElement.innerHTML = "";
 
- function render(){
-  let obj = {name:'The scream of the butterfly',author:'Vincent van gogh',price:1200, img:'img/3.jpeg'}
-  let rootElement = document.getElementById('cartForm');
-  rootElement.innerHTML = "";
-  
-  let elementPattern = car;
- elementPattern  = elementPattern.replace('|name|',obj.name);
- elementPattern =  elementPattern.replace('|price|',obj.price);
- elementPattern = elementPattern.replace('|imgPath|',obj.img);
- elementPattern = elementPattern.replace('|author|',obj.author);
+    let items = JSON.parse(window.localStorage.getItem("cartItems"));
 
-  for(let i=0; i<3; i++){
-    rootElement.innerHTML = rootElement.innerHTML + elementPattern;
+    if (items.length !== 0) {
+      document.getElementsByTagName("style")[0].innerHTML = cartAfter.replace(
+        "|amount|",
+        items.length
+      );
+    }
+
+    if (items.length > 3) {
+      items = items.slice(-3);
+    }
+    for (let i = 0; i < items.length; i++) {
+      let obj = items[i];
+      let res = elementPattern.replace("|price|", obj.price);
+      res = res.replace("|name|", obj.name);
+      res = res.replace("|author|", obj.author);
+      res = res.replace("|imgPath|", obj.img);
+      rootElement.innerHTML = rootElement.innerHTML + res;
+    }
+    rootElement.innerHTML =
+      rootElement.innerHTML +
+      '<button id="getAll" class="cartEl">VIEW ALL</button>';
   }
-  rootElement.innerHTML = rootElement.innerHTML + '<button id="getAll" class="cartEl">GET IT ALL!</button>';
- }
 
   return { init: init };
 })();
