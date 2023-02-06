f2c3db0b647ed31f2bd4703ac0e486c4444d5fa3
stanislav <rebel1866@yandex.ru>
2023-02-06 20:41:38 +0300

    removing from cart

--- js/cart.js
+++ js/cart.js
@@ -1,14 +1,19 @@
 let cart = (function () {
   const elementPattern =
-    '<div class="cartItem cartEl"><img class="cartImage cartEl" src="|imgPath|" >' +
+    '<div id="|itemid|" class="cartItem cartEl"><img class="cartImage cartEl" src="|imgPath|" >' +
     '<div class="cartTitle cartEl cartHeader">|name|</div> <div class="cartAuthor cartEl' +
     ' cartHeader">|author|</div><div class="cartPrice cartEl cartHeader">|price|$</div>' +
     ' <div class="manageBtns cartEl"><div class="mAmount cartEl"><div class="increase cartEl">' +
     '</div> <div class="amountItems cartEl">|amountItems|</div><div class="decrease cartEl"></div>' +
-    ' </div> <div class="removeItem cartEl">+</div> </div></div>';
+    ' </div> <div itemId="|itemid|" class="closeOuter cartEl"> <div itemId="|itemid|" class="closeInner cartEl">+</div></div></div></div>';
 
   const cartAfter =
     ' #cart::after {content: "|amount|";font-weight: bold;background: red;border-radius: 50%; width: 10px;   margin-top: -10px;margin-left: -30px; font-size: 11px;padding: 1px; padding-bottom:3px; padding-top:0; }';
+
+  const manageBtnsHtml =
+    '<div id="totalBlock" class="cartEl"><div id="totalHead" class="cartEl">TOTAL</div><div id="totC" class="cartEl">' +
+    '<div class="cartEl" id="totalItems">|amountItems| items</div> <div class="cartEl" id="totalPrice">|totalPrice|$</div></div>' +
+    ' <button id="getAll" class="cartEl">VIEW ALL</button>';
 
   function init() {
     document.getElementById("cartForm").style.display = "none";
@@ -31,7 +36,31 @@ let cart = (function () {
         document.getElementById("cartForm").style.display = "none";
       }
     });
+    document.getElementById("cartForm").addEventListener("click", (e) => {
+      if (
+        (e.target.classList !== null &&
+          e.target.classList.contains("closeOuter")) ||
+        e.target.classList.contains("closeInner")
+      ) {
+        let id = e.path[0].getAttribute("itemid");
+        document.getElementById(id).style.backgroundColor = "rgb(134, 21, 21)";
+        removeById(id);
+        setTimeout(() => {
+          render();
+        }, 400);
+      }
+    });
     render();
+  }
+  function removeById(id) {
+    let items = JSON.parse(window.localStorage.getItem("cartItems"));
+    let itemsNew = [];
+    for (const el of items) {
+      if (id != el.obj.id) {
+        itemsNew.push(el);
+      }
+    }
+    window.localStorage.setItem("cartItems", JSON.stringify(itemsNew));
   }
 
   function render() {
@@ -45,25 +74,30 @@ let cart = (function () {
         "|amount|",
         items.length
       );
+    } else {
+      document.getElementsByTagName("style")[0].innerHTML = "";
     }
 
     if (items.length > 3) {
       items = items.slice(-3);
     }
+    let amountItems = 0;
+    let totalPrice = 0;
     for (let i = 0; i < items.length; i++) {
       let obj = items[i].obj;
+      amountItems = amountItems + 1 * items[i].amount;
+      totalPrice = totalPrice + obj.price * items[i].amount;
       let res = elementPattern.replace("|price|", obj.price);
       res = res.replace("|name|", obj.name);
       res = res.replace("|author|", obj.author);
       res = res.replace("|imgPath|", obj.img);
+      res = res.replaceAll("|itemid|", obj.id);
       res = res.replace("|amountItems|", items[i].amount);
       rootElement.innerHTML = rootElement.innerHTML + res;
     }
-    rootElement.innerHTML =
-      rootElement.innerHTML +
-      '<div id="totalBlock" class="cartEl"><div id="totalHead" class="cartEl">TOTAL</div><div id="totC" class="cartEl">' +
-      '<div class="cartEl" id="totalItems">8 items</div> <div class="cartEl" id="totalPrice">2500$</div></div>' +
-      ' <button id="getAll" class="cartEl">VIEW ALL</button>';
+    let manRes = manageBtnsHtml.replace("|amountItems|", amountItems);
+    manRes = manRes.replace("|totalPrice|", totalPrice);
+    rootElement.innerHTML = rootElement.innerHTML + manRes;
   }
 
   return { init: init };
