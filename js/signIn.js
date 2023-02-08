3e576753acdc33d28d18e9bd642b92f60ba5ed79
stanislav <rebel1866@yandex.ru>
2023-02-08 22:19:46 +0300

    sign in validation

--- js/signIn.js
+++ js/signIn.js
@@ -11,6 +11,29 @@ let signIn = (function () {
     document.getElementById("close").addEventListener("click", () => {
       document.getElementById("signinF").style.display = "none";
     });
+    document.getElementById("doSignIn").addEventListener("click", () => {
+      let login = document.getElementById("login");
+      let password = document.getElementById("password");
+      if (login.value === "") {
+        login.style.border = "3px solid red";
+        login.placeholder = "This field can't be empty";
+      }
+      if (password.value === "") {
+        password.style.border = "3px solid red";
+        password.placeholder = "This field can't be empty";
+      }
+    });
+    document.getElementById("login").addEventListener("focus", () => {
+      let login = document.getElementById("login");
+      login.style.border = "";
+      login.placeholder = "Login";
+    });
+
+    document.getElementById("password").addEventListener("focus", () => {
+      let password = document.getElementById("password");
+      password.style.border = "";
+      password.placeholder = "Password";
+    });
   }
   return { init: init };
 })();
